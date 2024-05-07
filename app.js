e3f14455ae4112b5763f416cccdcdc10ab358319
gconbat7 <gildocode7@gmail.com>
2024-05-06 23:37:17 -0300

    Início do projeto Backend com Mysql

---
+++ app.js
@@ -0,0 +1,39 @@
+//Importar módulo express
+const express = require("express");
+
+//Importar módulo express-handlebars
+const { engine } = require("express-handlebars");
+
+//Importar Mysql
+const mysql = require("mysql2");
+
+//App
+const app = express();
+
+//Configuração do express-handlebars
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views')
+
+//Configuração de conexão
+const conexao = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"1983",
+    database:"projeto"
+});
+
+//Teste de conexão
+conexao.connect(function(erro){
+    if(erro) throw erro;
+    console.log("Conexão com servidor OK👍🏾");
+});
+
+
+//Rota principal
+app.get("/", function(req, res){
+    res.render("formulario");
+});
+
+//Servidor
+app.listen(8080);
