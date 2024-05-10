a1037a57277499ab55cb4d922a9f28601bd454ff
gconbat7 <gildocode7@gmail.com>
2024-05-09 22:21:18 -0300

    Criado, como adicionar prudutos e a lógica de alterar e remover

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 //Importar módulo express
 const express = require("express");
 
+//Importar módulo fileupload
+const fileupload = require("express-fileupload");
+
+
 //Importar módulo express-handlebars
 const { engine } = require("express-handlebars");
 
@@ -10,13 +14,26 @@ const mysql = require("mysql2");
 //App
 const app = express();
 
-//Adicionar bootstrap
+//Habilitando o upload de arquivos
+app.use(fileupload());
+
+//Adicionar Bootstrap
 app.use("/bootstrap", express.static("./node_modules/bootstrap/dist"));
+
+//Adicionar css
+app.use("/css", express.static("./css"));
+
+//Referenciar a pasta de imagens
+app.use("/imagens", express.static("./imagens"));
 
 //Configuração do express-handlebars
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views')
+
+//Manipulação de dados via rota
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 //Configuração de conexão
 const conexao = mysql.createConnection({
@@ -35,8 +52,41 @@ conexao.connect(function(erro){
 
 //Rota principal
 app.get("/", function(req, res){
-    res.render("formulario");
+    //SQL
+    let sql = "SELECT * FROM produtos";
+
+    //Executar comando SQL
+    conexao.query(sql, function(erro, retorno){
+        res.render("formulario", {produtos:retorno});
+    });
 });
+
+//Rota de cadastro
+app.post("/cadastrar", function(req, res){
+    //Obter os dados que serão utilizados para cadastro
+    let nome = req.body.nome;
+    let valor = req.body.valor;
+    let imagem = req.files.imagem.nome;
+
+    //SQL
+    let sql = `INSERT INTO produtos (nome, valor, imagem) VALUES ("${nome}", ${valor}, "${imagem}")`;
+
+    //Executar comando SQL
+    conexao.query(sql, function(erro, retorno){
+        //Caso ocorra algum erro
+        if(erro) throw erro;
+
+        //Caso ocorra o cadastro
+        req.files.imagem.mv(__dirname+"/imagens/"+req.files.imagem.name);
+        console.log(retorno);
+
+    });
+
+    //Retornar para a rota principal
+    res.redirect("/");
+});
+
+
 
 //Servidor
 app.listen(8080);
